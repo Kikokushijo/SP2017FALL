d1da74bd711aa2ee819cc37d3846f2e3a6f347a5
kikokushijo <zoo30215@gmail.com>
2018-01-01 00:49:39 +0800

    Revise File Reader

--- file_reader.c
+++ file_reader.c
@@ -3,29 +3,34 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <sys/types.h>
 
 int main(){
-    while (1){
-        // fprintf(stderr, "FORK!!!");
-        char filename[1024] = {};
-        read(STDIN_FILENO, filename, 1024);
-        filename[strlen(filename)] = '\0';
-        int fd = open(filename, O_RDONLY);
+    char filename[1024] = {};
+    read(STDIN_FILENO, filename, 1024);
 
-        if(fd < 0){
-            char tmp[1024] = {};
-            sprintf(tmp, "No such file or directory: %s\n", filename);
-            write(STDOUT_FILENO, tmp, strlen(tmp));
-            // perror(tmp);
-            exit(-1);
-        }
+    if (filename[strlen(filename) - 1] == '\n')
+        filename[strlen(filename) - 1] = 0;
 
-        for (;;){
-            char buffer[1024] = {};
-            int read_len = read(fd, buffer, 1020);
-            if (read_len == 0) break;
-            write(STDOUT_FILENO, buffer, strlen(buffer));
-        }
+    int fd = open(filename, O_RDONLY);
+
+    if(fd < 0){
+        char msg[1024] = {};
+        if (errno == EACCES)
+            sprintf(msg, "Permission Denied: %s\n", filename);
+        else if (errno == ENOENT)
+            sprintf(msg, "No Such File or Directory: %s\n", filename);
+        write(STDOUT_FILENO, msg, strlen(msg));
+        exit(-1);
     }
+
+    for (;;){
+        char buffer[1024] = {};
+        int read_len = read(fd, buffer, 1020);
+        if (read_len == 0) break;
+        write(STDOUT_FILENO, buffer, strlen(buffer));
+    }
+
+    exit(0);
 }
